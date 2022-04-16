799d4d777da23acc24158880585612a5fe18276e
shourovahmd <moh.abuhanifa@gmail.com>
2022-04-17 02:00:23 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Form } from "react-bootstrap";
+import "./App.css";
+
 
 function App() {
+  const handleSubmit = (event) => {event.preventDefault();
+  };
+  const handleEmailBlur = (event) => {console.log(event.target.value)};
+  const handlePasswordBlur = (event) => {console.log(event.target.value)};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <div onSubmit={handleSubmit} className="registration w-50 mx-auto mt-5">
+       <h2 className="text-primary">Please Register</h2>
+       <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+     </div>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpjNIitzWSpixVpYiBV7iP6hL6rRLFpEg",
+  authDomain: "react-firebase-assignment-09.firebaseapp.com",
+  projectId: "react-firebase-assignment-09",
+  storageBucket: "react-firebase-assignment-09.appspot.com",
+  messagingSenderId: "259844802630",
+  appId: "1:259844802630:web:b16e209796c36d3288e247"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
