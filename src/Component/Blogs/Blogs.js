1acb38237f7d468fc6700705870ec88bc4d8faf4
shourovahmd <moh.abuhanifa@gmail.com>
2022-04-18 20:42:51 +0600

    blogs

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Blogs.css';
 
 const Blogs = () => {
     return (
+        <div className='blogs'>
         <div>
-            <h2> Difference between authorization and authentication </h2>
+            <h2> Difference between authorization and authentication :</h2>
+            <p>Authentication is the process of verifying something or someone whether they are real or not. It could be a person, data, application, document, or anything. Authorization deals with the right of accessibility where it defines the right of someone or something. It tells whether a person or something can access, use or modify something else. In a computerized system, authentication confirms users are who they say they are, and authorization gives the user permission to access a resource.</p>
+        </div>
         </div>
     );
 };
